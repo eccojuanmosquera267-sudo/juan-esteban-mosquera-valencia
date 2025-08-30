b589a1a8351da446bc8f0b8c65b60e66e2230952
eccojuanmosquera267-sudo <e.cco.juan.mosquera267@cali.edu.co>
2025-08-30 11:49:19 -0500

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,13 @@
+Nombre = "juan"
+edad = 17
+equipo = "colombia"
+
+print(Nombre)
+print(edad)
+print(equipo)
+
+print(Nombre + str(edad) + equipo)
+
+print(Nombre, edad, equipo)
+
+print(f"Holaaa, mi nombre es {Nombre}, tengo {edad} años y soy de {equipo}")
