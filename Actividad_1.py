ad89dc7fe13bac34ccff03c9c57ba1ebe45ae3f2
eccojuanmosquera267-sudo <e.cco.juan.mosquera267@cali.edu.co>
2025-09-06 08:22:52 -0500

    Add files via upload

---
+++ Actividad_1.py
@@ -0,0 +1,95 @@
+# Actividad - 1
+
+# Curso CI/CD (Despliegue y automatización de aplicaciones)
+
+# Tema: Python Básico
+
+# Docente: Ing. Jean Paul Ordoñez Ibarguen
+
+# Fecha Entrega Máxima: 05/09/25
+
+# Forma entrega: Rama personal
+
+# Formato Actividad_1_Nombre_Apellido.py
+
+# ----------------------------------------------------------------
+
+
+
+"""
+Parte 1
+
+- Declare 2 variables de tipo String (Cadena o texto) y luego imprima cada una en consola
+
+- Declare 2 variables de tipo entero (int) y luego imprima cada una en consola
+
+- Con las variables de tipo String, realiza la concatenación de ambas, imprimiendo el resultado en consola.
+
+- Con las variables de tipo entero, realiza la suma, resta, multiplicación, división, división entera, módulo entre ambas. Recuerde imprimir los resultados en consola.
+
+"""
+
+# PONER SOLUCIÓN PARTE 1
+
+
+
+
+
+
+"""
+Parte 2
+
+- Defina un arreglo de números y letras con longitud mínima de 5 elementos
+- Imprima el arreglo en consola.
+- Imprimaa el primer elemento del arreglo por posición
+- Imprimaa el último elemento del arreglo por posición
+- Imprima la suma entre el primer valor del elemento y el último (Recuerde tomarlo por nombreArreglo[Posición])
+- Imprima la resta entre el primer valor del elemento y el último (Recuerde tomarlo por nombreArreglo[Posición])
+- Imprima la multiplicación entre el primer valor del elemento y el último (Recuerde tomarlo por nombreArreglo[Posición])
+
+"""
+
+# PONER SOLUCIÓN PARTE 1
+
+
+
+"""
+Parte 3
+
+Responda. ¿Qué es CI/CD?
+
+R/ son siglas de integracion continua entrega y despliegue continuo
+
+Responda. ¿Qué herramientas hay para desplegar aplicaciones?
+
+R/ AWS Google cloud platform GCP sistemas de integracion continua CI/CD
+
+Responda. ¿Qué herramientas hay para automatizar despliegues?
+
+R/ Jenkins Gitlab CI Githu actions Azure DevOps Ansible Docker AWS CodeDeploy
+
+Responda. ¿Qué herramientas en python para automatizar pruebas?
+R/ test runner Playwright Appium BDD 
+
+"""
+
+
+"""
+Parte 4 
+
+- Cree un repositorio personal donde va a gestionar sus despliegues y proyectos
+- Cree una carpeta en su maquina donde almacenara su repositorio creado
+- Clone el repositorio dentro de la carpeta
+- Modifique su archivo README poniendo el enlace de su repositorio
+
+
+(Tenga en cuenta):
+
+• El repositorio debe ser público
+• El repositorio debe tener su respectivo README
+• Debe adjuntar el enlace en este documento
+
+ENLACE A MI REPOSITORIO: 
+
+"""
+
